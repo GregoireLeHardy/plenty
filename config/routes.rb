9fa1161a66a1788c1af5027c81409c76ef9227aa
Gregoire le Hardy <gregoire.lehardy@gmail.com>
2020-02-01 15:19:27 +0100

    more quick fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   get 'pages/home'
   resources :articles
   resources :users, only: [:show, :new, :create]
-  resources :bikes do
-    resources :bookings, only: [:show, :new, :create, :index, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
