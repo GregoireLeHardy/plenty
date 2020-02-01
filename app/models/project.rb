261f679e530dfc7926e7917034282166bec0fb00
Gregoire le Hardy <gregoire.lehardy@gmail.com>
2020-02-01 17:08:23 +0100

    description limit

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,7 @@ class Project < ApplicationRecord
   has_many :users, through: :follower_projects
   has_many :categories, through: :project_categories
   has_many :payments, through: :donations
+
+  validates :short_description, length: { maximum: 255 }
+  validates :long_description, length: { maximum: 1500 }
 end
