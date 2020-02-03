342dca52f366f6a0a176b140a663a67d4fa49b8c
Julian Sautter <juliansautter1@gmail.com>
2020-02-03 16:27:56 +0100

    completing project.create flow with redirection to project.show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,17 +1,17 @@
-class ProjectsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+ class ProjectsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
+    find_project
   end
 
   def create
     @project = Project.new(project_params_user)
     if @project.save!
-      redirect_to projects_path
+      redirect_to project_path(@project)
     else
     end
   end
@@ -20,4 +20,17 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+private
+
+  def project_params_user
+    project_params.merge(user_id: current_user.id)
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :short_description, :long_description, :photo)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
 end
