58359c4b29fce546d9799c1681e0a1bedf01908a
Julian Sautter <juliansautter1@gmail.com>
2020-02-04 19:54:10 +0100

    Add validations to project model + photo field

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :articles
   has_many :donations
+  has_one_attached :photo
   has_many :follower_projects
   has_many :project_categories
   has_many :users, through: :follower_projects
@@ -10,4 +11,6 @@ class Project < ApplicationRecord
 
   validates :short_description, length: { maximum: 255 }
   validates :long_description, length: { maximum: 1500 }
+  validates :name, presence: true
+  validates :photo, presence: true
 end
