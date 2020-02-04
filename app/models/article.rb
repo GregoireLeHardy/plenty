98b07adaf53604bfc500b52d80628ddbebe5c9e1
Gregoire le Hardy <gregoire.lehardy@gmail.com>
2020-02-04 20:03:28 +0100

    update validation articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   has_one_attached :photo
   belongs_to :project
+
+  validates :photo, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
 end
