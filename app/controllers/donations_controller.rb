0f0204397f7f61fb28f2edf9d760478e9ad84418
Julian Sautter <juliansautter1@gmail.com>
2020-02-10 23:01:47 +0100

    Completion of payment flow with WEBHOOK configured for heroku page

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,23 +1,23 @@
 class DonationsController < ApplicationController
   def create
     project = Project.find(params[:project_id])
-    order = Donation.create!(project: project, ptoject_sku: project.sku, amount: project.price, state: 'pending', user: current_user)
+    donation = Donation.create!(project: project, project_sku: project.sku, price_cents: project.price, state: 'pending', user: current_user)
 
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
-        name: project.sku,
-        images: [project.photo_url],
+        name: project.name,
+        images: [project.photo],
         amount: project.price_cents,
         currency: 'eur',
         quantity: 1
       }],
-      success_url: order_url(order),
-      cancel_url: order_url(order)
+      success_url: donation_url(donation),
+      cancel_url: donation_url(donation)
     )
 
-    order.update(checkout_session_id: session.id)
-    redirect_to new_order_payment_path(order)
+    donation.update(checkout_session_id: session.id)
+    redirect_to new_donation_payment_path(donation)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create]
   resources :projects do
     resources :articles, only: [:show, :index, :create, :new, :update, :delete]
-    resources :donations, only: [:show, :create] do
-      resources :payments, only: :new
-    end
+  end
+  resources :donations, only: [:show, :create] do
+    resources :payments, only: :new
   end
   mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
