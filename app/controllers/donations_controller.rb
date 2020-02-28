9070302078ec6baa296b8149d489b27172a33822
Julian Sautter <juliansautter1@gmail.com>
2020-02-28 23:02:31 +0100

    Adding project and projects link to payment confirmation

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -23,7 +23,6 @@ class DonationsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:donation][:project_id])
     @donation = current_user.donations.find(params[:id])
     @project = @donation.project
     authorize @donation
